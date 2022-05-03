958da533f0fba1791c20ce1bcf67ffd5054b60d2
Necrofairy <96979246+Necrofairy@users.noreply.github.com>
2022-05-03 12:44:49 +0300

    Добавил корзину
    Добавлена корзина, осталось кнопку купить добавить

--- assignments 1/insta3/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:insta3/widgets/app.dart';
-
-void main() {
-  runApp(App());
-}
--- assignments 2/market/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:market/pages/main_page/main_page.dart';
-
-void main() {
-  runApp(const MainPage(
-
-  ));
-}
-
